0b487fb110b616db6e657288695871e811aec6a6
Ali A. Dhillon <aleedhx@gmail.com>
2022-01-03 02:22:14 +0500

    setup the inertia and a welcome page

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Welcome To {{ appName }}</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Welcome',
+    props: {
+        appName: String
+    }
+}
+</script>
